232fe2eb0a4856b91c02b75c251e1653639a10ef
Rabiul Hossen <rhsimon69@gmail.com>
2023-12-15 22:04:58 +0600

    module practise

---
+++ practise-module.py
@@ -0,0 +1,17 @@
+import turtle #turtle used for graphical interface
+bob = turtle.Turtle()
+print(bob)
+turtle.mainloop() #mainloop tells the window to wait for the user to do something
+""""
+bob.fd(100) #bob.fd is a method
+bob.lt(80) 
+bob.fd(100)"""
+# single line comment """ multi line comment
+
+for i in range(4):
+    bob.fd(100)
+    wn = turtle.Screen()
+wn.bgcolor("light green")
+wn.title("Turtle")
+skk = turtle.Turtle()
+
