f3ea4e09b4b6c573c98c19fc375e08ffbce98ecc
Rabiul Hossen <rhsimon69@gmail.com>
2023-12-15 22:50:24 +0600

    star and square drawing

--- practise-module.py
+++ practise-module.py
@@ -8,10 +8,34 @@ bob.lt(80)
 bob.fd(100)"""
 # single line comment """ multi line comment
 
+"""
 for i in range(4):
     bob.fd(100)
     wn = turtle.Screen()
 wn.bgcolor("light green")
 wn.title("Turtle")
 skk = turtle.Turtle()
+"""
 
+#draw square  using Turtle Programming
+import turtle 
+skk = turtle.Turtle()
+
+for i in range(4):
+	skk.forward(50)
+	skk.right(90)
+	
+turtle.done()
+
+
+# draw star
+
+star= turtle.Turtle()
+star.right(75)
+star.forward(100)
+for j in range(4):
+    star.right(144)
+    star.forward(100)
+    
+turtle.done()
+    
